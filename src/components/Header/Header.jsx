b8f97bc620fba541191f50f7d9be78ada4356874
sakib-xrz <sakibxrz21@gmail.com>
2022-10-03 16:34:19 +0600

    Scroll down button animation added

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,24 +3,27 @@ import CTA from './CTA'
 import "./Header.css"
 import MyImg from "./../../assets/me.png"
 import HeaderSocial from './HeaderSocial'
+import {AiOutlineDoubleRight} from "react-icons/ai"
 
 const Header = () => {
   return (
-    <header id='header'>
+    <header id="header">
       <div className="container header_container">
         <h5>Hello I'm</h5>
         <h1>Md Sakibul Islam</h1>
         <h5 className="text-light">Front-End Developer</h5>
-        <CTA/>
-        <HeaderSocial/>
+        <CTA />
+        <HeaderSocial />
         <div className="me">
           <img src={MyImg} alt="" />
         </div>
 
-        <a className='scroll_down' href="#contact">Scroll Down</a>
+        <a className="scroll_down" href="#contact">
+          Scroll Down <AiOutlineDoubleRight className="bounce" />
+        </a>
       </div>
     </header>
-  )
+  );
 }
 
 export default Header
