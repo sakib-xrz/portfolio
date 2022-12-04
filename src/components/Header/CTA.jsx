ee684081837aade6c828893e31862422b28e2a97
sakib-xrz <sakibxrz21@gmail.com>
2022-12-05 02:29:01 +0600

    resume button fix

--- src/components/Header/CTA.jsx
+++ src/components/Header/CTA.jsx
@@ -5,7 +5,7 @@ import CV from "./../../assets/sakibul-islam-resume.pdf"
 const CTA = () => {
   return (
     <div className='cta'>
-        <a className='btn' href={CV} download>Download CV</a>
+        <a className='btn' href={CV} download>Download Resume</a>
         <a className='btn btn-primary' href="#contact">Let's Talk</a>
     </div>
   )
